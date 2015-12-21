76b78028c00b96fdb04a49433be7fcb8b0b76f9e
ty2471979 <tysutherland@gmail.com>
2015-12-21 13:20:26 -0800

    Create dragonGameLoops.js

---
+++ dragonGameLoops.js
@@ -0,0 +1,27 @@
+var slaying = true;
+var youHit = Math.floor(Math.random() * 2);
+var damageThisRound = Math.floor(Math.random() * 5 + 1);
+var totalDamage = 0;
+
+    while (slaying)     {
+        if (youHit) {
+            console.log("You hit the dragon and did " + damageThisRound + " damage!");
+            totalDamage += damageThisRound;
+                if (totalDamage >= 4) {
+                console.log("You did it! You slew the dragon!");
+                slaying = false;
+                    } 
+                else {
+                youHit = Math.floor(Math.random() * 2);
+                     }
+                }
+        
+        else {
+            console.log("The dragon burninated you and the peasants!");
+            slaying = false;
+        }
+        
+        
+    
+    }
+    
